5d16e0c06cf252d6ae5c519b0cbd9681e4a1b886
Luis Daniel <lopezvalenciald@gmail.com>
2026-05-29 16:39:17 -0700

    uso de variables para operaciones basicas

--- app/src/main/java/com/example/baseskotlin/tiposDeDatos.kt
+++ app/src/main/java/com/example/baseskotlin/tiposDeDatos.kt
@@ -13,6 +13,25 @@ fun main(){
     val genderSymbol: Char = 'M'
     val weight: Double = 2.3
 
-    println("El bebe $firstName $lastName de genero $gender nacio el dia $day del mes $month del año $year")
+    //println("El bebe $firstName $lastName de genero $gender nacio el dia $day del mes $month del año $year")
     //El bebe Luis Daniel Lopez Valencia de genero Masculino nacio el dia 28 del mes Junio del a�o 1997
+    operacionesNumericas()
+}
+
+fun operacionesNumericas(){
+
+    val num1: Int = 20
+    val num2: Int = 30
+    val num3: Int = 10
+
+    val suma: Int = num1+num2+num3
+    println(suma) //resultado 60
+    val resta: Int = num2-num3-num1
+    println(resta) //resultado 0
+    val division:Int = num2/num3
+    println(division) //resultado 3
+    val multiplicacion: Int =num1*num2*num3
+    println(multiplicacion) //resultado 6000
+    val modulo: Int = num2%num1
+    println(modulo) //resultado 10
 }
