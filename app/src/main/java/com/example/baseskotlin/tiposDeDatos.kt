ae9238f3432fb1826a254d2e7a32aac9988f6c66
Luis Daniel <lopezvalenciald@gmail.com>
2026-05-29 16:24:31 -0700

    primer print y uso de variables para un texto

--- app/src/main/java/com/example/baseskotlin/tiposDeDatos.kt
+++ app/src/main/java/com/example/baseskotlin/tiposDeDatos.kt
@@ -1,2 +1,18 @@
 package com.example.baseskotlin
 
+//variables
+fun main(){
+
+    val firstName: String = "Luis Daniel"
+    val lastName: String = "Lopez Valencia"
+    val month: String = "Junio"
+    val year: Int = 1997
+    val day: Int = 28
+    val gender: String = "Masculino"
+    val crybaby: Boolean = true
+    val genderSymbol: Char = 'M'
+    val weight: Double = 2.3
+
+    println("El bebe $firstName $lastName de genero $gender nacio el dia $day del mes $month del año $year")
+    //El bebe Luis Daniel Lopez Valencia de genero Masculino nacio el dia 28 del mes Junio del a�o 1997
+}
